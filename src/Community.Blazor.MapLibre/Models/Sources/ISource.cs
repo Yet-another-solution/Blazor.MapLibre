9e86ae572893e9ac71015b70ffc5d9619b5ddfc9
MatejMa2ur <matejmazur@outlook.com>
2025-07-22 15:58:16 +0200

    feat: extend ISource with additional properties
    - Add properties for attribution, id, zoom levels, tile clipping, and vector layer IDs.
    - Support enhanced configuration of map sources.
    
    This update introduces several new properties to the `ISource` interface, providing greater flexibility and control over map data sources. These changes enhance functionality, including the support for zoom level settings, tile behaviors, and vector layer identification.

--- src/Community.Blazor.MapLibre/Models/Sources/ISource.cs
+++ src/Community.Blazor.MapLibre/Models/Sources/ISource.cs
@@ -17,5 +17,65 @@ public interface ISource
     /// Defines the source type (e.g., vector, raster, geojson, etc.).
     /// </summary>
     [JsonPropertyName("type")]
-    public string Type { get; }
+    string Type { get; }
+
+    /// <summary>
+    /// The attribution for the source.
+    /// </summary>
+    [JsonPropertyName("attribution")]
+    string? Attribution { get; set; }
+
+    /// <summary>
+    /// The id for the source. Must not be used by any existing source.
+    /// </summary>
+    [JsonPropertyName("id")]
+    string Id { get; set; }
+
+    /// <summary>
+    /// `false` if tiles can be drawn outside their boundaries, `true` if they cannot.
+    /// </summary>
+    [JsonPropertyName("isTileClipped")]
+    bool? IsTileClipped { get; set; }
+
+    /// <summary>
+    /// The maximum zoom level for the source.
+    /// </summary>
+    [JsonPropertyName("maxzoom")]
+    int MaxZoom { get; set; }
+
+    /// <summary>
+    /// The minimum zoom level for the source.
+    /// </summary>
+    [JsonPropertyName("minzoom")]
+    int MinZoom { get; set; }
+
+    /// <summary>
+    /// `true` if tiles should be sent back to the worker for each overzoomed zoom level, `false` if not.
+    /// </summary>
+    [JsonPropertyName("reparseOverscaled")]
+    bool? ReparseOverscaled { get; set; }
+
+    /// <summary>
+    /// `true` if zoom levels are rounded to the nearest integer in the source data, `false` if they are floor-ed to the nearest integer.
+    /// </summary>
+    [JsonPropertyName("roundZoom")]
+    bool? RoundZoom { get; set; }
+
+    /// <summary>
+    /// The canonical tile ID for the source.
+    /// </summary>
+    [JsonPropertyName("tileID")]
+    string? TileID { get; set; }
+
+    /// <summary>
+    /// The tile size for the source.
+    /// </summary>
+    [JsonPropertyName("tileSize")]
+    int TileSize { get; set; }
+
+    /// <summary>
+    /// The vector layer IDs for the source.
+    /// </summary>
+    [JsonPropertyName("vectorLayerIds")]
+    string[]? VectorLayerIds { get; set; }
 }
