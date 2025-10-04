a82a3bd0770739bfcc1ad24caba42fe89d10deb2
Ryan Spain <8723433+ryanspain@users.noreply.github.com>
2025-10-04 18:48:53 +0100

    Update base URL handling and stylesheet/script paths for Blazor app

--- docs/template/public/main.js
+++ docs/template/public/main.js
@@ -3,18 +3,19 @@ export default {
 
         // Create a base element to set the base URL for the Blazor app
         let baseElement = document.createElement('base')
-        baseElement.href = '/';
+        const isLocalhost = window.location.hostname === "localhost";
+        baseElement.href = isLocalhost ? "/" : "/Blazor.MapLibre/";
         document.head.appendChild(baseElement);
         
         // Create a link element to import stylesheets
         let linkElement = document.createElement('link');
         linkElement.rel = 'stylesheet';
-        linkElement.href = '/_content/Community.Blazor.MapLibre/maplibre-5.3.0.min.css';
+        linkElement.href = '_content/Community.Blazor.MapLibre/maplibre-5.3.0.min.css';
         document.head.appendChild(linkElement);
         
         // Create a script element to load the Blazor WebAssembly runtime
         let blazorScriptElement = document.createElement('script');
-        blazorScriptElement.src = '/_framework/blazor.webassembly.js';
+        blazorScriptElement.src = '_framework/blazor.webassembly.js';
         blazorScriptElement.autostart = 'true';
         document.body.appendChild(blazorScriptElement);
     },
